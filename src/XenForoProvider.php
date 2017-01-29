59e062614970ce1318ab2e25d0066e76e5824843
rgbvader <admin@rgbvader.com>
2017-01-29 00:42:05 -0500

    Initial commit

---
+++ src/XenForoProvider.php
@@ -0,0 +1,33 @@
+<?php
+namespace Rgbvader\OAuth;
+
+use League\OAuth2\Client\Provider\GenericProvider;
+
+class XenForoProvider extends GenericProvider
+{
+    const ACCESS_TOKEN_RESOURCE_OWNER_ID = 'user_id';
+
+    /**
+     * @param string $url
+     * @param string $query
+     * @return string
+     */
+    protected function appendQuery($url, $query)
+    {
+        if (substr($url, strlen($url)- 1, 1) == '&')
+        {
+            return $url.$query;
+        }
+        return parent::appendQuery($url, $query);
+    }
+
+    /**
+     * Generate a random state, but do it publicly
+     * @param int $length
+     * @return string
+     */
+    public function getRState($length = 32)
+    {
+        return parent::getRandomState($length);
+    }
+}
