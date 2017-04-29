a50964e935c34050bbaf300623b17645715f8da2
Zach <z61@zeesixtyone.com>
2017-04-29 03:08:04 -0400

    Added User resource owner

---
+++ src/User.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zach
+ * Date: 4/29/2017
+ * Time: 2:59 AM
+ */
+
+namespace Rgbvader\OAuth;
+
+
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+
+class User implements ResourceOwnerInterface
+{
+    /** @var  User Id */
+    protected $data = array();
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function toArray()
+    {
+        return $this->data;
+    }
+
+    public function __get($name)
+    {
+        if (array_key_exists($name, $this->data)) {
+            return $this->data[$name];
+        }
+
+        return null;
+    }
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+}
